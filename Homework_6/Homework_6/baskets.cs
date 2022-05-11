bfd2c472267689dc0feaa0d3c554b1c3e789b03b
Tymur Kukhtin <mr.timur.pipal@gmail.com>
2022-05-11 22:21:26 +0300

    ready shop

---
+++ Homework_6/Homework_6/baskets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    internal class baskets
+    {
+        public int number_of_order;
+        public product[] products = new product[10];
+
+        public void Write_basket()
+        {
+            int sum = 0;
+            for(int i = 0; i < this.products.Length; i++)
+            {
+                if (this.products[i] != null)
+                {
+                    Console.WriteLine($"name:{this.products[i].name} price:{this.products[i].price} comment:{this.products[i].comment}");
+                    sum += this.products[i].price;
+                }
+            }
+            Console.WriteLine($"To pay:{sum}");
+        }
+
+    }
+}
---
+++ Homework_6/Homework_6/order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    internal class order
+    {
+        public baskets[] basket1 = new baskets[10];
+        public void Make_an_order()
+        {
+            baskets basket = new baskets();
+            product[] products = new product[10];
+            product product1 = new product();
+            product1.Make_a_product_list(products);
+            int amount = new Random().Next(1, 6);
+            for(int i = 0; i < amount; i++)
+            {
+                int number = new Random().Next(0, 10);
+                basket.products[i] = products[number];
+            }
+            this.basket1.Prepend(basket);
+            this.Callback(basket);
+        }
+
+        public void Callback(baskets basket1)
+        {
+            Console.WriteLine("Your order is ready\nYou ordered:");
+            basket1.Write_basket();
+        }
+    }
+}
